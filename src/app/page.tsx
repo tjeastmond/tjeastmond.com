74b4f12cfc0972824bccd344af732abde063c2b4
TJ Eastmond <tj.eastmond@gmail.com>
2024-08-20 17:13:17 -0400

    Added social icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
+import Socials from "@components/socials";
+
 export default function Home() {
   return (
     <div className="container">
       <div className="main-content">
         <h1>&gt; tjeastmond_</h1>
         <p>This is some sample text to show the layout.</p>
-        <div className="social-media">
-          <a href="#" className="social-icon">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="#" className="social-icon">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="#" className="social-icon">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </div>
+        <Socials />
       </div>
     </div>
   );
