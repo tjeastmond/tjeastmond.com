e8a4b855e8b4422a49db92dd79bad407aebed407
TJ Eastmond <tj.eastmond@gmail.com>
2024-09-16 11:29:49 -0400

    Buy Me a Coffee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,18 @@ export default function Home() {
       <div className="main-content">
         <Title />
         <p>
-          You've stumbled upon the homepage of TJ Eastmond: Girl Dad, Husband, Gamer, Marvel SNAP
-          player, Engineering Manager, Software Engineer, and Data Engineer.
+          You've stumbled upon the homepage of TJ Eastmond: Girl Dad, Husband,
+          Gamer, Marvel SNAP player, Engineering Manager, Software Engineer, and
+          Data Engineer.
         </p>
         <p>
-          Shoot me an <Link url="mailto:tj.eastmond@gmail.com" text="email" />.
+          Shoot me an <Link url="mailto:tj.eastmond@gmail.com" text="email" />,
+          or{" "}
+          <Link
+            url="https://buymeacoffee.com/tjeastmond"
+            text="Buy Me a Coffee"
+          />
+          .
         </p>
         <Socials />
       </div>
